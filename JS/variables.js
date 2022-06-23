280d4e3cca2bfd70f15c3620ebf9a55ff9f6d17e
fer1ado <fer1ado@yahoo.com.ar>
2022-06-23 15:37:47 -0300

    DesafioClase6_V1.0

---
+++ JS/variables.js
@@ -0,0 +1,30 @@
+
+const catalogo = document.querySelector("#disProd")
+const carrito = document.querySelector("#VentProd")
+const Nprod = document.querySelector("#NProd")
+const PrecioFinal = document.querySelector("#PrecioFinal")
+const contador1 =  document.getElementById("contador1")
+const contador2 =  document.getElementById("contador2")
+
+
+
+const ventasRealizdas= []
+
+const piezasEnstock = []
+
+const carroVentas = []
+
+const stockActualizado = []
+
+
+
+
+
+
+
+
+
+
+
+
+
